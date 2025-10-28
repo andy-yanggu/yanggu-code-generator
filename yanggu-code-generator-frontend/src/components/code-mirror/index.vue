107d73fac82a1912e1772ec00a3e21d794735632
杨顾 <17621950294@163.com>
2025-10-28 17:01:12 +0800

    代码组件，使用defineModel进行优化

--- yanggu-code-generator-frontend/src/components/code-mirror/index.vue
+++ yanggu-code-generator-frontend/src/components/code-mirror/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<Codemirror
-		v-model="innerValue"
+		v-model="model"
 		placeholder="Code goes here..."
 		:autofocus="true"
 		:indent-with-tab="true"
@@ -11,47 +11,23 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 import { Codemirror } from 'vue-codemirror'
 import { javascript } from '@codemirror/lang-javascript'
 import { EditorView } from '@codemirror/view'
 import { oneDarkTheme } from '@codemirror/theme-one-dark'
 import { useAppStore } from '@/store/app-store'
 
+const model = defineModel<string>()
+
 const props = defineProps({
-	modelValue: {
-		type: String,
-		default: ''
-	},
 	readOnly: {
 		type: Boolean,
 		default: false
 	}
 })
 
-const emit = defineEmits(['update:modelValue'])
-
 const appStore = useAppStore()
-
-// 内部值，和外部 v-model 双向绑定
-const innerValue = ref(props.modelValue)
-
-// 监听外部传入的值，保持同步
-watch(
-	() => props.modelValue,
-	val => {
-		if (val !== innerValue.value) {
-			innerValue.value = val
-		}
-	}
-)
-
-// 监听内部编辑器内容变化，通知父组件更新
-watch(innerValue, val => {
-	if (val !== props.modelValue) {
-		emit('update:modelValue', val)
-	}
-})
 
 // codemirror 的只读样式
 const customStyle = EditorView.theme({
