0019aa2aa9f67764dde1ff46b102a566f7ed462b
yanggu <17621950294@qq.com>
2026-09-09 23:43:58 +0800

    feat(backend): 项目查询条件扩展及分页MapStruct重构

--- yanggu-code-generator-backend/src/main/java/com/yanggu/code/generator/common/mapstruct/PageVOMapstruct.java
+++ yanggu-code-generator-backend/src/main/java/com/yanggu/code/generator/common/mapstruct/PageVOMapstruct.java
@@ -8,13 +8,13 @@ import com.yanggu.code.generator.common.domain.vo.PageVO;
  *
  * @param <V> vo实体类泛型（controller返回参数）
  */
-public interface PageVOMapstruct<V> extends PageVOOtherMapstruct {
+public interface PageVOMapstruct<V> {
 
     /**
      * 将VO分页数据转换成PageVO分页数据
      */
     default PageVO<V> voToPageVO(PageQuery<V> voPageQuery) {
-        return toPageVO(voPageQuery);
+        return new PageVO<>(voPageQuery);
     }
 
 }
--- yanggu-code-generator-backend/src/main/java/com/yanggu/code/generator/domain/query/ProjectVOQuery.java
+++ yanggu-code-generator-backend/src/main/java/com/yanggu/code/generator/domain/query/ProjectVOQuery.java
@@ -8,6 +8,7 @@ import lombok.EqualsAndHashCode;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * 项目VO查询实体类
@@ -19,5 +20,29 @@ public class ProjectVOQuery extends PageQuery<ProjectVO> implements Serializable
 
     @Serial
     private static final long serialVersionUID = 1L;
+
+    /**
+     * 项目名称
+     */
+    @Schema(description = "项目名称")
+    private String projectName;
+
+    /**
+     * 生成方式
+     */
+    @Schema(description = "生成方式")
+    private Integer generatorType;
+
+    /**
+     * 开始时间
+     */
+    @Schema(description = "开始时间")
+    private Date startTime;
+
+    /**
+     * 结束时间
+     */
+    @Schema(description = "结束时间")
+    private Date endTime;
 
 }
